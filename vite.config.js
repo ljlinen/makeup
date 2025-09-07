cd45d68d956298997093aa1b0287b74893409bdf
unknown <SiphoMoloto0@gmail.com>
2025-09-07 22:36:19 +0200

    Configured vite base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/makeup/",
   plugins: [react(), svgr({
     // svgr options: https://react-svgr.com/docs/options/
     svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
